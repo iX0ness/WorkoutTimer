0efdfcf1db8eeefa7eb0576ea852239ac230edc8
Mykhaylo Levchuk <mykhaylo.levchuk@fieldcode.com>
2020-08-14 14:04:51 +0200

    add project to repo

---
+++ WorkoutTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoutTimer
+//
+//  Created by Mykhaylo Levchuk on 14/08/2020.
+//  Copyright © 2020 Mykhaylo Levchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
