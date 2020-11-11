042c6c346b299d2df5007a1b7c163797f28a53d6
Mykhaylo Levchuk <mykhaylo.levchuk@fieldcode.com>
2020-11-11 17:36:57 +0100

    working use cases: 1) 1 lap with 1 round 2) one lap with few rounds

--- WorkoutTimer/Screens/MainScreen/TimerMainViewController.swift
+++ WorkoutTimer/Screens/MainScreen/TimerMainViewController.swift
@@ -10,27 +10,66 @@ import UIKit
 import RxSwift
 
 class TimerMainViewController: UIViewController, Storyboarded {
-
+    
     weak var coordinator: MainCoordinator?
-    var subscription: Disposable?
+    var workoutSubscription: Disposable?
+    var restSubscription: Disposable?
     let disposeBag = DisposeBag()
     
-    var time = 10
-
+    var rounds = 3
+    var workoutTime = 10
+    var restTime = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
         
-        subscription = Observable<Int>.interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
-            .map { self.time - $0 }
+//        let workout = runTimer(with: workoutTime, for: "workout time")
+//        let rest = runTimer(with: restTime, for: "rest time")
+//        Observable.concat([workout, rest]).subscribe { element in
+//            print("Element: \(element)")
+//        } onCompleted: {
+//            print("completed")
+//        } onDisposed: {
+//            print("Disposed")
+//        }.disposed(by: disposeBag)
+
+        configureActionsAndRests(laps: 1, rounds: 2, roundTime: 5, restTime: 3)
+            .subscribe { time in
+                print("time: \(time)")
+            } onCompleted: {
+                print("Completed")
+            } onDisposed: {
+                print("Disposed")
+            }.disposed(by: disposeBag)
+
+    }
+    
+    func runTimer(with time: Int) -> Observable<Int> {
+        return Observable<Int>.interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
+            .map { time - $0 }
             .takeUntil(.inclusive, predicate: { $0 == 0 })
-            .subscribe(onNext: { value in
-                print(value)
-            }, onCompleted: {
-                print("completed")
-            })
-        subscription?.disposed(by: disposeBag)
     }
 
+    
+    func configureActionsAndRests(laps: Int, rounds: Int, roundTime: Int, restTime: Int) -> Observable<Int> {
+        switch (laps, rounds) {
+        case (1, 1):
+            return runTimer(with: roundTime)
+            
+        case (1, 1...):
+            let result = (0..<rounds)
+                .map { _ in return [runTimer(with: roundTime), runTimer(with: restTime)] }
+                .flatMap { $0 }
+                .dropLast()
+            return Observable.concat(result)
+            
+            
+            
+        default:
+           return  Observable.of(2)
+        }
+    
+    }
 }
 
