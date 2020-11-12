7303bbf3dd1dcdf88d681b113f1d6d25c85b86c6
Mykhaylo Levchuk <mykhaylo.levchuk@fieldcode.com>
2020-11-12 20:11:53 +0100

    counting for any amount of laps and rounds

--- WorkoutTimer/Screens/MainScreen/TimerMainViewController.swift
+++ WorkoutTimer/Screens/MainScreen/TimerMainViewController.swift
@@ -12,29 +12,14 @@ import RxSwift
 class TimerMainViewController: UIViewController, Storyboarded {
     
     weak var coordinator: MainCoordinator?
-    var workoutSubscription: Disposable?
-    var restSubscription: Disposable?
-    let disposeBag = DisposeBag()
     
-    var rounds = 3
-    var workoutTime = 10
-    var restTime = 5
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
         
-//        let workout = runTimer(with: workoutTime, for: "workout time")
-//        let rest = runTimer(with: restTime, for: "rest time")
-//        Observable.concat([workout, rest]).subscribe { element in
-//            print("Element: \(element)")
-//        } onCompleted: {
-//            print("completed")
-//        } onDisposed: {
-//            print("Disposed")
-//        }.disposed(by: disposeBag)
-
-        configureActionsAndRests(laps: 1, rounds: 2, roundTime: 5, restTime: 3)
+        configureActionsAndRests(laps: 2, rounds: 2, roundTime: 2, restTime: 1)
             .subscribe { time in
                 print("time: \(time)")
             } onCompleted: {
@@ -56,20 +41,33 @@ class TimerMainViewController: UIViewController, Storyboarded {
         switch (laps, rounds) {
         case (1, 1):
             return runTimer(with: roundTime)
-            
         case (1, 1...):
-            let result = (0..<rounds)
-                .map { _ in return [runTimer(with: roundTime), runTimer(with: restTime)] }
-                .flatMap { $0 }
-                .dropLast()
-            return Observable.concat(result)
-            
-            
+            let workoutConfig = createPairedActionRestLap(rounds: rounds, roundTime: roundTime, restTime: restTime).dropLast()
+            return Observable.concat(workoutConfig)
+        case (2, 1...):
+            let workoutConfig = createMultiLapWorkoutConfig(laps: laps, rounds: rounds, roundTime: roundTime, restTime: restTime).dropLast()
+            return Observable.concat(workoutConfig)
             
         default:
-           return  Observable.of(2)
+           fatalError(
+            """
+            Unexpected number of laps or rounds
+            Laps: \(laps) | Rounds: \(rounds)
+            """)
         }
     
+    }
+    
+    func createPairedActionRestLap(rounds: Int, roundTime: Int, restTime: Int) -> [Observable<Int>] {
+        return (0..<rounds)
+            .map { _ in return [runTimer(with: roundTime), runTimer(with: restTime)] }
+            .flatMap { $0 }
+    }
+    
+    func createMultiLapWorkoutConfig(laps: Int, rounds: Int, roundTime: Int, restTime: Int) -> [Observable<Int>] {
+        return (0..<laps)
+            .map { _ in return createPairedActionRestLap(rounds: rounds, roundTime: roundTime, restTime: restTime) }
+            .flatMap { $0 }
     }
 }
 
